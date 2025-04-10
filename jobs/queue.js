a2f50d1d6a5ede3246c11e808634e8fb407374ea
reshmapr1999 <reshmarajan1907@gmail.com>
2025-04-10 12:29:05 +0400

    redis

--- jobs/queue.js
+++ jobs/queue.js
@@ -1,5 +1,12 @@
+
 const kue = require('kue');
-const queue = kue.createQueue();
+require('dotenv').config();
+const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+console.log(process.env.REDIS_URL);
+const queue = kue.createQueue({
+  redis: redisUrl
+});
+
 const resetMailer = require('../mailers/reset_password_mailer');
 
 queue.process('reset_email', function(job, done) {
